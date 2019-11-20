d90eeb35c20a0cddb28f7615d0613ca6bf0e3c49
Fei <52169317+FeiY74@users.noreply.github.com>
2019-11-20 16:21:55 +0000

    added solution for element is not clickable error

--- run-headless-test.py
+++ run-headless-test.py
@@ -3,3 +3,10 @@ from selenium.webdriver.chrome.options import Options
 op = Options()
 op.add_argument("--headless")
 driver = webdriver.Chrome(options=op)
+
+#if it returns error like "....is not clickable at point", try use these:
+op = Options()
+op.add_argument("--window-size=1920,1080")
+op.add_argument("--start-maximized")
+op.add_argument("--headless")
+driver = webdriver.Chrome(options=op)
